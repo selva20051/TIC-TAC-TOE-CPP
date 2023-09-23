19e8703c3ceab5364bb6c24d472054dc9c33001a
Manuqlly <selvaperumal20005@gmail.com>
2023-09-23 18:50:09 +0530

    TIC-TAC-TOE.CPP

---
+++ tictactoe.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
+
+int main()
+{
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    drawBoard(spaces);
+
+    return 0;
+}
+
+void drawBoard(char *spaces)
+{
+    std::cout<<"     |     |     "<<'\n';
+    std::cout<<"   "<< spaces[0] <<" |  "<< spaces[1] <<"  |  "<< spaces[2] <<"   "<<'\n';
+    std::cout<<"_____|_____|_____"<<'\n';
+    std::cout<<"     |     |     "<<'\n';
+    std::cout<<"   "<< spaces[3] <<" |  "<< spaces[4] <<"  |  "<< spaces[5] <<"   "<<'\n';
+    std::cout<<"_____|_____|_____"<<'\n';
+    std::cout<<"     |     |     "<<'\n';
+    std::cout<<"   "<< spaces[6] <<" |  "<< spaces[7] <<"  |  "<< spaces[8] <<"   "<<'\n';
+    std::cout<<"     |     |     "<<'\n';
+
+}
+void playerMove(char *spaces, char player)
+{
+
+
+}
+void computerMove(char *spaces, char computer)
+{
+
+
+}
+bool checkWinner(char *spaces, char player, char computer)
+{
+
+    return 0;
+}
+bool checkTie(char *spaces)
+{
+
+
+    return 0;
+}
